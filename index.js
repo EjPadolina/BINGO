bb95d1c379292a8fb3b1f5462cf658e7f1fb70e8
EjPadolina <157558087+EjPadolina@users.noreply.github.com>
2024-03-19 13:38:46 +0800

    Index.js

--- index.js
+++ index.js
@@ -1,26 +1,77 @@
-generateNumber(15);
+console.log("Hello World");
+let B = false,
+    I = false,
+    N = false,
+    G = false,
+    O = false;
 
-function generateNumber(max){    
-    let randomNumber = Math.floor(Math.random() *max) + 1;
 
-    if (randomNumber <= 15){
-        console.log('the Number is in B.');
-    }
-    else if(randomNumber >= 16 && randomNumber <= 30){
-        console.log('the number is in I');
-    }
-    else if(randomNumber >= 31 && randomNumber <= 45){
-        console.log('the number is in N');
-    }
-    else if(randomNumber >= 46 && randomNumber <= 60){
-        console.log('the number is in G');
-    }
-    else if(randomNumber >= 61 && randomNumber <= 75){
-        console.log('the number is in O');
-    }
-    else{
-        console.log('the number is invalid');
+    let BNum, INum, NNumber, GNumber, ONumber;
+
+
+function newNumber(maxNumber) {
+    // Addition = "+"
+    // Subtraction = "-"
+    // Multiplication = "*"
+    // Division = "/"
+    // Remainder = "%"
+    let x = Math.floor(Math.random() * maxNumber) + 1;
+
+    // if...else if...else statement
+    // Conditional operator:
+    // GTE > =, LTE < =, EQ ==, NEQ ! =, AND &, OR ||
+    if (x <= 15) {
+        console.log(New number ${x} belongs to "B");
+        B = true;
+        BNumber = x;
+    } else if (x >= 16 && x <= 30) {
+        console.log(New number ${x} belongs to "I"); 
+        I = true;
+        INumber = x;
+    } else if (x >= 31 && x <=45) {
+        console.log(New number ${x} belongs to "N");
+        N = true; 
+        NNumber = x;
+    } else if (x >= 46 && x <= 60) { 
+        console.log(New number ${x} belongs to "G");
+        G = true;
+        GNumber = x;
+    } else if (x >= 61 && x <=75) {
+        console.log(New number ${x} belongs to "O");
+        O = true;
+        ONumber = x;
     }
 
-    return console.log(randomNumber);
+     if (B == true && I == true && N == true && G == true && O == true) {
+        console.log(BINGOOO!!!);
+        console.table({
+            B: BNumber,
+            I: INumber,
+            N: NNumber,
+            G: GNumber,
+            O: ONumber,
+        });
+    }
 }
+
+newNumber(75);
+
+// LOOPS
+let counter = 5;
+// for (counter; counter >= 0; counter --) {
+   // console.log(Current counter is ${counter});
+
+// if (counter == 2) break;
+    
+// newNumber(75);
+    
+// }
+
+// while (counter >= 0) {
+   // console.log(While, Current counter is ${counter});
+  // counter--;
+ // }
+ 
+ while (B == false ||  I == false || N == false ||  G == false ||  O == false ) {
+    newNumber(75);
+   }
